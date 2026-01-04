114f692000fff5bd9989e0e865449b21f5589ecc
bhavyasribrahmani <bhavyasribrahmani@gmail.com>
2026-01-04 18:47:17 +0530

    List basics

---
+++ list.py
@@ -0,0 +1,22 @@
+l = [2,3,4,5,6,9,7,8]
+
+print(l[0])
+print(l[3])
+print(l[-1])
+print(l[1:7])
+g = [5,2,4,8,1]
+g[0] = 3
+print(g)
+u = [55,38,75,89,12,35,67,25]
+u.sort()
+print(u)
+f = [1,2,3,4]
+f.append(5)
+f.insert(0,12)
+print(f)
+t = [2,4,3,1]
+r = t.index(4)
+print(r)
+y = input().split()
+l = [(int(item))**2 for item in y]
+print(l)
