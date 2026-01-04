f61ac3340e0cb0fa37c44d16e5707bca178978c7
bhavyasribrahmani <bhavyasribrahmani@gmail.com>
2026-01-04 18:22:57 +0530

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,38 @@
+# own logic,vision version 
+# # first problem
+print("1.")
+a = 2034785
+b = 1999999
+c = a+b
+print("total value of c after adding a & b:",c)
+d = 756480
+e = c+d
+print("the final result after all operations:",e)
+print(" ")
+# second problem
+print("2.")
+t = 9
+y = 3*t
+z = 5*y + 15
+print("the value of y:",y)
+print("the value of z:",z)
+print(" ")
+# third problem:if i double the number of pens in my backpack and add 5,i get 23.so how many pens do i have in my backpack?
+print("3.")
+t = 23
+u = t-5
+r = 2*u
+print("the number of pens in my backpack:",r)
+print(" ")
+#fourth problem:89 chocolates shared within 7 members.how many complete chocolates do each get?
+print("4.")
+d = 89
+e = 89 // 7
+print("complete chocolates one get after sharing:",int(e))
+print(" ")
+#fifth problem:i do have 55% pizza left after sharing it with my 7friends but mine is double then others.how much percentage of pizza left after distributing equally between my friends?
+print("5.")
+a = 100
+b = 55
+c =(100 -55)%7
+print("amount of pizza left:",c)
